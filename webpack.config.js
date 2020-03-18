3c97f3474af22bb02dff5cbfc5a8f2e9cc40d13f
zce <w@zce.me>
2020-03-18 11:21:29 +0800

    feat: 客户端基本结构

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  mode: 'none',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader' 
+      }
+    ]
+  }
+}
